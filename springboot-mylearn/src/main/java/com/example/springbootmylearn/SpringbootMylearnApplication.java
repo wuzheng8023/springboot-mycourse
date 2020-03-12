b73f23a15539a513021478d631e134e7967c008a
wuzheng <1345381396@qq.com>
2020-03-12 12:39:00 +0800

    项目构建初始化以及数据源配置

---
+++ springboot-mylearn/src/main/java/com/example/springbootmylearn/SpringbootMylearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMylearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMylearnApplication.class, args);
+    }
+
+}
---
+++ springboot-mylearn/src/test/java/com/example/springbootmylearn/SpringbootMylearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMylearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
